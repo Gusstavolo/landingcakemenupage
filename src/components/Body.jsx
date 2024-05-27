72def7b912cc88f0e566d5133f28c09d11eb7494
Gustavo Oli <69ggustavoo69@gmail.com>
2024-05-27 14:17:45 -0300

    second commit

---
+++ src/components/Body.jsx
@@ -0,0 +1,19 @@
+import '../App.css'
+
+
+export const Body = () => {
+
+
+    return(
+        <div className="body_main">
+            <div className='banner'></div>
+
+            
+            <div className='botton_wtts_main'>
+                <div className='botton_wtts'> 
+
+                </div>
+            </div>
+        </div>
+    )
+}
